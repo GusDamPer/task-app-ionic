18d097d8f21f081d1922a7f406ee084e287d21f7
GusDamPer <gusdamper@gmail.com>
2023-07-11 20:53:55 -0300

    Primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlMJJCY8OhgECyMokoGalNplOfly9dsNM",
+    authDomain: "task-app-626e0.firebaseapp.com",
+    projectId: "task-app-626e0",
+    storageBucket: "task-app-626e0.appspot.com",
+    messagingSenderId: "310549148423",
+    appId: "1:310549148423:web:609745c636018e60538ba2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlMJJCY8OhgECyMokoGalNplOfly9dsNM",
+    authDomain: "task-app-626e0.firebaseapp.com",
+    projectId: "task-app-626e0",
+    storageBucket: "task-app-626e0.appspot.com",
+    messagingSenderId: "310549148423",
+    appId: "1:310549148423:web:609745c636018e60538ba2"
+  }
 };
 
 /*
